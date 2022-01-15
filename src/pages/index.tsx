5794eef3befdd9c638189f87e5093b24874b131d
FukeKazki <kazkichi0906@gmail.com>
2022-01-15 11:51:35 +0900

    Fix: 必要のない文言を削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
-import { css } from "@emotion/react";
 import React from "react";
 import { useAuth } from "~/hook/useAuth";
-
-const title = css`
-  color: gray;
-`;
 
 const Home: React.FC = () => {
   const { auth, signUp, signUpError, signUpIsLoading } = useAuth();
@@ -12,8 +7,6 @@ const Home: React.FC = () => {
 
   return (
     <>
-      <div css={title}>syogi-camera</div>
-      <div>将棋カメラ</div>
       {signUpError && <p>エラー: {signUpError}</p>}
       <button disabled={signUpIsLoading} onClick={() => signUp("github")}>
         ログイン

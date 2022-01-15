c114b1502433e902d97d2b89f1f146fff9f8042d
take-cantik <miki.takeru508@mail.kyutech.jp>
2022-01-15 13:40:35 +0900

    Add: GitHubIcon and edit login button

--- src/component/shared/buttons/LoginButton.tsx
+++ src/component/shared/buttons/LoginButton.tsx
@@ -7,17 +7,18 @@ export interface LoginButtonProps extends ComponentPropsWithRef<"button"> {
 }
 
 const common = css`
-  height: 64px;
-  padding: 0 68px 0 8px;
+  height: 108px;
+  padding: 0 176px 0 12px;
   color: ${colors.black.lighten[1]};
   background: ${colors.white};
   display: flex;
   align-items: center;
   justify-content: center;
+  gap: 80px;
   border: 1px solid ${colors.black.lighten[2]};
-  border-radius: 32px;
-  font-size: 2rem;
-  font-weight: 500;
+  border-radius: 54px;
+  font-size: 3.6rem;
+  font-weight: 600;
   transition: 0.3s;
   cursor: pointer;
 
